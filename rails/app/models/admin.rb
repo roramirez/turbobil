d6c918ea56ffb6f89b9c03ba74b9bce502018907
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-09-23 21:26:57 -0300

    removed method for devise (admin and customer) not yet implemented

--- rails/app/models/admin.rb
+++ rails/app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
     self.table_name = 'admin'
 
 
--- rails/app/models/customer.rb
+++ rails/app/models/customer.rb
@@ -1,8 +1,8 @@
 class Customer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, 
+         :rememberable, :trackable, :validatable
     include CustomerHelper
     self.table_name = 'customer'
 
