0b0de0d00f1c92997ce7ed55dd68773fb447934e
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-09-29 08:11:35 -0300

    delete example graph data

--- rails/app/assets/javascripts/customer.js
+++ rails/app/assets/javascripts/customer.js
@@ -3,7 +3,6 @@
 //= require sb-admin-2/plugins/metisMenu/metisMenu.min.js
 //= require sb-admin-2/plugins/morris/raphael.min.js
 //= require sb-admin-2/plugins/morris/morris.min.js
-//= require sb-admin-2/plugins/morris/morris-data.js
 //= require sb-admin-2/sb-admin-2.js
 //= require jquery.datetimepicker
 //= require jquery_ujs
