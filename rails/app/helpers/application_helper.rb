b2028362d788dbfbe9c508332b74b2939c914755
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-10-09 22:27:27 -0300

    gravatar as service and add no_avatar, fix url gravatar

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,17 +1,15 @@
 module ApplicationHelper
 
-  def gravatar_icon(email, size = nil)
-    if email.present?
-      size = 40 if size.nil? || size <= 0
-
-      gravatar_url = "http://www.gravatar.com/avatar/#{hash}?s=%{size}&d=identicon"
-      sprintf gravatar_url,
-        hash: Digest::MD5.hexdigest(email.strip.downcase),
-        size: size,
-        email: email.strip
-    end
+  def avatar_icon(email, size = nil)
+    gravatar_icon(email, size)
   end
 
+  def gravatar_icon(email, size = nil)
+    GravatarService.new.execute(email, size) || default_avatar
+  end
 
+  def default_avatar
+    image_path('no_avatar.png')
+  end
 
 end
