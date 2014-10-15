f69ca8f57546090791518373cd226c177635f684
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-10-14 23:38:19 -0300

    add index to call table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904024147) do
+ActiveRecord::Schema.define(version: 20141014020434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 20140904024147) do
     t.text     "dialstatus"
     t.float    "price_for_customer"
   end
+
+  add_index "call", ["at", "admin_id"], name: "idx_at_admin_on_call", using: :btree
+  add_index "call", ["at", "customer_id"], name: "idx_at_customer_on_call", using: :btree
 
   create_table "codec", force: true do |t|
     t.text "name"
