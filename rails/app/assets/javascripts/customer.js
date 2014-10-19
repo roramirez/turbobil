939598564beebbd71cc7ee7972cdd6a18a64ab34
Rodrigo Ramírez Norambuena <decipher.hk@gmail.com>
2014-10-19 13:00:02 -0300

    add morris gem

--- rails/app/assets/javascripts/customer.js
+++ rails/app/assets/javascripts/customer.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require bootstrap
 //= require sb-admin-2/plugins/metisMenu/metisMenu.min.js
-//= require sb-admin-2/plugins/morris/raphael.min.js
-//= require sb-admin-2/plugins/morris/morris.min.js
+//= require raphael
+//= require morris
 //= require sb-admin-2/sb-admin-2.js
 //= require jquery.datetimepicker
 //= require jquery_ujs
